a8e0248be70c0e5ec8c1f6d87e98176edd743437
unknown <karnvish@umich.edu>
2013-12-15 17:29:49 -0500

    creating users form

--- app/Controller/CribsnapController.php
+++ app/Controller/CribsnapController.php
@@ -4,20 +4,22 @@ App::import('Vendor', 'Snapchat', array('file' => 'php-snapchat/src/snapchat.php
 
 class CribsnapController extends AppController {
 
-
 public function Cribsnap() {
+		$username = $this->data['User']['snapchatUser'];
 		
 		$snapchat = new Snapchat('kvishy', 'pass0602');
 
 		$id = $snapchat->upload(
 			Snapchat::MEDIA_IMAGE,
-			file_get_contents('https://s3-us-west-2.amazonaws.com/cribspot-img/listings/lrg_528fb90fdf5a8.jpg')
+			file_get_contents('https://s3-us-west-2.amazonaws.com/cribspot-img/listings/lrg_52828e46a79fc.jpg')
 		);
 
 
 		print_r($id);
-		$snapchat->send($id, array('kvishy'), 8);
-		$this->Session->setFlash(__('message sent', true));
+		if ($snapchat->send($id, array('kvishy'), 8));
+		{
+				$this->Session->setFlash(__('message sent', true));
+		}
 
 }
 
